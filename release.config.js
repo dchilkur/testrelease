5c5c9c24127b65dcbb624dcc9bd2cb590170acf5
Dinesh Chilkuri <dchilkuri@salesforce.com>
2023-04-20 09:54:22 -0700

    initial commit

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    branches: ['main', { name: 'hotfix', prerelease: true }, { name: 'hfx-er', prerelease: true }],
+    tagFormat: 'v${version}',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/github',
+            {
+                successComment: false,
+                failComment: false,
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                message: 'chore(release): ${nextRelease.gitTag} [skip ci]',
+            },
+        ],
+    ],
+};
