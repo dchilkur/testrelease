eb2ce9bf9d1fb731e6d611bccb245411133d5421
Dinesh Chilkuri <dchilkuri@salesforce.com>
2023-05-24 15:30:13 -0700

    feat: testing multiple projects

---
+++ packages/clientweb/release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', { name: 'hotfix', prerelease: true }, { name: 'hfx-er', prerelease: true } , {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
+    tagFormat: '@dchilkur/clientweb@${version}',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/github',
+            {
+                successComment: false,
+                failComment: false,
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                message: 'chore(release): ${nextRelease.gitTag} [skip ci]',
+            },
+        ],
+    ],
+};
--- packages/testproj1/release.config.js
+++ packages/testproj1/release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', { name: 'hotfix', prerelease: true }, { name: 'hfx-er', prerelease: true } , {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
-    tagFormat: 'v${version}',
+    tagFormat: '@dchilkur/tesproj1@${version}',
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
